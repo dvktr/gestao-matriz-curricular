04b985c2e877c25d018885f75c4fd6f2cfe2e3df
luanmas <luanmas27@gmail.com>
2024-09-27 19:34:23 -0300

    dev: hidden sidebar on mobile screen

--- src/components/Sidebar/index.tsx
+++ src/components/Sidebar/index.tsx
@@ -5,7 +5,7 @@ import { lightLine, logo } from "@/assets";
 
 export default function Sidebar() {
   return (
-    <aside className="w-64 bg-primary h-screen p-4 text-white flex flex-col justify-between fixed">
+    <aside className="md:w-64 bg-primary h-screen p-4 text-white md:flex flex-col justify-between fixed hidden">
       <div className="flex flex-col items-start space-y-4">
         <Image alt="logo" src={logo} />
         <Image alt="light-line" src={lightLine} />
