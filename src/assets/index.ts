3fcf66549ac75057fb43163b02b9d34d3e74fefa
Victor Mendes <victormendes04012004@gmail.com>
2024-09-27 15:50:23 -0300

    insert need-help assets

---
+++ src/assets/index.ts
@@ -0,0 +1,4 @@
+export { default as needHelpBG } from '@/assets/need-help-bg.svg'
+export { default as needHelpIcon } from '@/assets/need-help-icon.svg'
+export { default as logo } from '@/assets/logo.svg'
+export { default as lightLine } from '@/assets/light-line.svg'
