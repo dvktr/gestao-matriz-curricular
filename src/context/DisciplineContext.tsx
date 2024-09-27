41b678eb475a2a7a2a74a20a12d928112c762624
Victor Mendes <victormendes04012004@gmail.com>
2024-09-27 16:08:46 -0300

    configure discipline context

--- src/context/DisciplineContext.tsx
+++ src/context/DisciplineContext.tsx
@@ -1,4 +1,3 @@
-// DisciplineContext.tsx
 import React, { createContext, useContext, useState, useEffect } from 'react';
 import { IDiscipline } from '@/types/discipline';
 
@@ -10,22 +9,31 @@ interface DisciplineContextType {
 const DisciplineContext = createContext<DisciplineContextType | undefined>(undefined);
 
 export const DisciplineProvider: React.FC<{ children: React.ReactNode }> = ({ children }) => {
-    const [selectedDisciplines, setSelectedDisciplines] = useState<IDiscipline[]>(() => {
+  const [selectedDisciplines, setSelectedDisciplines] = useState<IDiscipline[]>([]);
+
+  useEffect(() => {
+    // Garante que o localStorage seja acessado apenas no cliente
+    if (typeof window !== "undefined") {
       const storedData = localStorage.getItem('selectedDisciplines');
-      return storedData ? JSON.parse(storedData) : [];
-    });
-  
-    useEffect(() => {
+      if (storedData) {
+        setSelectedDisciplines(JSON.parse(storedData));
+      }
+    }
+  }, []);
+
+  useEffect(() => {
+    // Atualiza o localStorage quando selectedDisciplines mudar
+    if (typeof window !== "undefined") {
       localStorage.setItem('selectedDisciplines', JSON.stringify(selectedDisciplines));
-    }, [selectedDisciplines]);
-  
-    return (
-      <DisciplineContext.Provider value={{ selectedDisciplines, setSelectedDisciplines }}>
-        {children}
-      </DisciplineContext.Provider>
-    );
-  };
-  
+    }
+  }, [selectedDisciplines]);
+
+  return (
+    <DisciplineContext.Provider value={{ selectedDisciplines, setSelectedDisciplines }}>
+      {children}
+    </DisciplineContext.Provider>
+  );
+};
 
 export const useDisciplineContext = () => {
   const context = useContext(DisciplineContext);
