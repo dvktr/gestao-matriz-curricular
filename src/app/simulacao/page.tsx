03ba0c27ce8d881a6169b03441f56cf2153820f2
Victor Mendes <victormendes04012004@gmail.com>
2024-09-28 19:08:22 -0300

    dev: set PDFDownloadLink

--- src/app/simulacao/page.tsx
+++ src/app/simulacao/page.tsx
@@ -1,14 +1,38 @@
+// pages/simulacao.tsx
 "use client";
 
 import SimulationGrid from "@/components/SimulationGrid";
 import Timetable from "@/components/Timetable";
-import { DisciplineProvider } from "@/context/DisciplineContext"; // Importe o contexto
+import { Button } from "@/components/ui/button";
+import { DisciplineProvider } from "@/context/DisciplineContext";
+import { Printer } from "lucide-react";
+import { PDFDownloadLink } from '@react-pdf/renderer';
+import { PDFTimetable } from '@/components/PDFTimetable';
 
 export default function Simulacao() {
   return (
     <DisciplineProvider>
       <div className="flex md:flex-row flex-col md:justify-around md:p-2 md:overflow-hidden">
-        <Timetable />
+        <div className="flex-col items-start">
+          <PDFDownloadLink
+            document={
+              // O DisciplineProvider está aqui para garantir que o PDF também tenha acesso ao contexto
+              <DisciplineProvider>
+                <PDFTimetable />
+              </DisciplineProvider>
+            }
+            fileName="grade-horarios.pdf"
+            className="text-black gap-2 font-bold mb-2 flex items-center"
+          >
+            {({ loading }) => (
+              <Button>
+                <Printer />
+                {loading ? "Gerando PDF..." : "Compartilhar"}
+              </Button>
+            )}
+          </PDFDownloadLink>
+          <Timetable isPrint={false} />
+        </div>
         <SimulationGrid />
       </div>
     </DisciplineProvider>
