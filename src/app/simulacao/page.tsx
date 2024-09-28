84620f7b1b7ddda77881772b77153926e675eba2
Victor Mendes <victormendes04012004@gmail.com>
2024-09-28 19:19:20 -0300

    dev: adjust PDFDownloadlink to use only in client side

--- src/app/simulacao/page.tsx
+++ src/app/simulacao/page.tsx
@@ -1,4 +1,3 @@
-// pages/simulacao.tsx
 "use client";
 
 import SimulationGrid from "@/components/SimulationGrid";
@@ -6,31 +5,39 @@ import Timetable from "@/components/Timetable";
 import { Button } from "@/components/ui/button";
 import { DisciplineProvider } from "@/context/DisciplineContext";
 import { Printer } from "lucide-react";
-import { PDFDownloadLink } from '@react-pdf/renderer';
-import { PDFTimetable } from '@/components/PDFTimetable';
+import { PDFDownloadLink } from "@react-pdf/renderer";
+import { PDFTimetable } from "@/components/PDFTimetable";
+import { useEffect, useState } from "react";
 
 export default function Simulacao() {
+  const [isClient, setIsClient] = useState(false); // Adicione isso
+  useEffect(() => {
+    setIsClient(true); // Indica que estamos no cliente
+  }, []);
   return (
     <DisciplineProvider>
       <div className="flex md:flex-row flex-col md:justify-around md:p-2 md:overflow-hidden">
         <div className="flex-col items-start">
-          <PDFDownloadLink
-            document={
-              // O DisciplineProvider está aqui para garantir que o PDF também tenha acesso ao contexto
-              <DisciplineProvider>
-                <PDFTimetable />
-              </DisciplineProvider>
-            }
-            fileName="grade-horarios.pdf"
-            className="text-black gap-2 font-bold mb-2 flex items-center"
-          >
-            {({ loading }) => (
-              <Button>
-                <Printer />
-                {loading ? "Gerando PDF..." : "Compartilhar"}
-              </Button>
-            )}
-          </PDFDownloadLink>
+          {isClient && (
+            <PDFDownloadLink
+              document={
+                // O DisciplineProvider está aqui para garantir que o PDF também tenha acesso ao contexto
+                <DisciplineProvider>
+                  <PDFTimetable />
+                </DisciplineProvider>
+              }
+              fileName="grade-horarios.pdf"
+              className="text-black gap-2 font-bold mb-2 flex items-center"
+            >
+              {({ loading }) => (
+                <Button>
+                  <Printer />
+                  {loading ? "Gerando PDF..." : "Compartilhar"}
+                </Button>
+              )}
+            </PDFDownloadLink>
+          )}
+
           <Timetable isPrint={false} />
         </div>
         <SimulationGrid />
